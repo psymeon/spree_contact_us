a9cc81e92b99eb387e38d5c9ee660a08a1b5eed9
psymeon <petros.symeonidis@gmail.com>
2014-10-03 15:22:28 +0300

    Update contacts_controller.rb

--- app/controllers/spree/contact_us/contacts_controller.rb
+++ app/controllers/spree/contact_us/contacts_controller.rb
@@ -4,10 +4,7 @@ class Spree::ContactUs::ContactsController < Spree::StoreController
   def create
     @contact = Spree::ContactUs::Contact.new(params[:contact_us_contact])
 
-    logger.info @contact
-    logger.info @contact.last_name.nil?
-    logger.info @contact.last_name.empty?
-    if (@contact.last_name.nil? or @contact.last_name.empty?)
+    if (params[:contact_us_contact][:last_name].nil? or params[:contact_us_contact][:last_name].empty?)
       if @contact.save
         if Spree::ContactUs::Config.contact_tracking_message.present?
           flash[:contact_tracking] = Spree::ContactUs::Config.contact_tracking_message
