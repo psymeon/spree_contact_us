95a916a25e7d2ae5ba9a06ccce6989fd5a553fe9
psymeon <petros.symeonidis@gmail.com>
2014-10-03 14:16:28 +0300

    Update contact.rb

--- app/models/spree/contact_us/contact.rb
+++ app/models/spree/contact_us/contact.rb
@@ -10,7 +10,7 @@ module Spree
       validates :email,   :format => { :with => /@/i },
                           :presence => true
       validates :message, :presence => true
-      validates :last_name, :presence => false
+      validates :last_name, :length => {:is => 0 }
       validates :name,    :presence => {:if => Proc.new{SpreeContactUs.require_name}}
       validates :subject, :presence => {:if => Proc.new{SpreeContactUs.require_subject}}
 
